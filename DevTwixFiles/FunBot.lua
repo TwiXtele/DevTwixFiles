1336269116e7b90a4b3095af14b18efac5f3f7c3
Soerce TeAm TwiX <85749573+TwiXtele@users.noreply.github.com>
2021-09-20 17:35:54 +0300

    Add files via upload

--- DevTwixFiles/FunBot.lua
+++ DevTwixFiles/FunBot.lua
@@ -20,33 +20,6 @@ Dev_HmD(msg.chat_id_, msg.id_, 1, text, 1, "md")
 end
 end
 
-if msg.reply_to_message_id_ ~= 0 then
-if text and text:match("^رفع مطي$") and not DevHmD:get(DevTwix..'HmD:Lock:Stupid'..msg.chat_id_) and ChCheck(msg) then
-function donky_by_reply(extra, result, success)
-if DevHmD:sismember(DevTwix..'User:Donky:'..msg.chat_id_, result.sender_user_id_) then
-ReplyStatus(msg,result.sender_user_id_,"Reply","☨︙رافعه مطي وشادلة عربانة 😂🌚") 
-else
-ReplyStatus(msg,result.sender_user_id_,"Reply","☨︙تم رفعه في قائمة المطايه") 
-DevHmD:sadd(DevTwix..'User:Donky:'..msg.chat_id_, result.sender_user_id_)
-end end
-getMessage(msg.chat_id_, msg.reply_to_message_id_,donky_by_reply)
-end end
----------------------------------------------------------------------------------------
-if msg.reply_to_message_id_ ~= 0  then
-if text and text:match("^تنزيل مطي$") and not DevHmD:get(DevTwix..'HmD:Lock:Stupid'..msg.chat_id_) and ChCheck(msg) then
-function donky_by_reply(extra, result, success)
-if not DevHmD:sismember(DevTwix..'User:Donky:'..msg.chat_id_, result.sender_user_id_) then
-ReplyStatus(msg,result.sender_user_id_,"Reply","☨︙هو ليس مطي ليتم تنزيله") 
-else
-DevHmD:srem(DevTwix..'User:Donky:'..msg.chat_id_, result.sender_user_id_)
-ReplyStatus(msg,result.sender_user_id_,"Reply","☨︙تم تنزيله من قائمة المطايه") 
-end end
-getMessage(msg.chat_id_, msg.reply_to_message_id_,donky_by_reply)
-end 
-end
-end 
-end
-
 end
 return {
 DevTwix = Fun
